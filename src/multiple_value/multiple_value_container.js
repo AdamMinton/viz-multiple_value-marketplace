e55f06db0a2be7715cb836fb4c03b38d8529e80d
AdamMinton <53316110+AdamMinton@users.noreply.github.com>
2021-12-01 10:41:58 -0500

    removed comparison removing the datapoint

--- src/multiple_value/multiple_value_container.js
+++ src/multiple_value/multiple_value_container.js
@@ -93,9 +93,33 @@ looker.plugins.visualizations.add({
       })
     });
 
+    //BUG: Need to figure out how to get from measure name and label into this format
+    const fields_to_select = [
+    {'Average Cost':'order_items.average_cost'},
+    {'Average Spend Per Customer':'order_items.average_spend_per_customer'},
+    {'Count of Orders':'order_items.count_of_orders'},
+    {'Count with Scatters':'order_items.count_with_scatter'},
+    {'Average Sale Price':'order_items.average_sale_price'},
+    {'Average Gross Revenue':'order_items.average_gross_revenue'},
+    {'Days since latest order':'order_items.days_since_lastest_order'},
+    {'Item Return Rate':'order_items.item_return_rate'},
+    {'Number of Items Returned':'order_items.number_of_items_returned'},
+    ]
+      //measures.map(measure => {
+      // console.log(measure.name);
+      // console.log(measure.label);
+      // let string = `'{${measure.name}':'${measure.label}'`
+      // console.log(string);
+      // let array = JSON.parse(string)
+      // console.log(array)
+      // return(
+      //   fields_to_select[`${measure.name}`] = `${measure.label}`
+      // )
+    //})
+
     const options = Object.assign({}, baseOptions)
-    
     dataPoints.forEach((dataPoint, index) => {
+      //console.log(dataPoint.name);
       //Style -- apply to all
       options.dividers = {
         type: 'boolean',
@@ -112,7 +136,7 @@ looker.plugins.visualizations.add({
         section: 'Style',
         order: 1,
       }
-      if (config[`show_comparison_${dataPoint.name}`] !== true) {
+      //if (config[`show_comparison_${dataPoint.name}`] !== true) {
         options[`style_${dataPoint.name}`] = {
           type: `string`,
           label: `${dataPoint.label} - Color`,
@@ -204,82 +228,87 @@ looker.plugins.visualizations.add({
             order: 10 * index + 9 + 1
           }
         }
-      }
+      //}
       // Comparison - all data points other than the first
-      if (index >= 1) {
-        options[`show_comparison_${dataPoint.name}`] = {
-          type: 'boolean',
-          label: `${dataPoint.label} - Show as comparison`,
+      options[`show_comparison_${dataPoint.name}`] = {
+        type: 'boolean',
+        label: `${dataPoint.label} - Show as comparison`,
+        section: 'Comparison',
+        default: false,
+        order: 10 * index,
+      }
+      if (config[`show_comparison_${dataPoint.name}`] === true) {
+        options[`field_to_compare_${dataPoint.name}`] = {
+          type: 'string',
+          display: 'select',
+          label: `${dataPoint.label} - Style`,
+          values: fields_to_select,
           section: 'Comparison',
-          default: false,
-          order: 10 * index,
+          order: 10 * index + 1,
         }
-
-        if (config[`show_comparison_${dataPoint.name}`] === true) {
-          options[`comparison_style_${dataPoint.name}`] = {
-            type: 'string',
-            display: 'radio',
-            label: `${dataPoint.label} - Style`,
-            values: [
-              {'Show as Value': 'value'},
-              {'Show as Percentage Change': 'percentage_change'},
-              {'Calculate Progress': 'calculate_progress'},
-              {'Calculate Progress (with Percentage)': 'calculate_progress_perc'},
-            ],
-            section: 'Comparison',
-            default: 'value',
-            order: 10 * index + 1,
-          }
-          options[`comparison_show_label_${dataPoint.name}`] = {
+        options[`comparison_style_${dataPoint.name}`] = {
+          type: 'string',
+          display: 'radio',
+          label: `${dataPoint.label} - Style`,
+          values: [
+            {'Show as Value': 'value'},
+            {'Show as Percentage Change': 'percentage_change'},
+            {'Calculate Progress': 'calculate_progress'},
+            {'Calculate Progress (with Percentage)': 'calculate_progress_perc'},
+          ],
+          section: 'Comparison',
+          default: 'value',
+          order: 10 * index + 1,
+        }
+        options[`comparison_show_label_${dataPoint.name}`] = {
+          type: 'boolean',
+          label: `${dataPoint.label} - Show Label`,
+          section: 'Comparison',
+          default: true,
+          order: 10 * index + 3,
+        }
+        if (config[`comparison_style_${dataPoint.name}`] === "percentage_change") {
+          options[`pos_is_bad_${dataPoint.name}`] = {
             type: 'boolean',
-            label: `${dataPoint.label} - Show Label`,
+            label: `Positive Values are Bad`,
             section: 'Comparison',
-            default: true,
-            order: 10 * index + 3,
+            default: false,
+            order: 10 * index + 2,
           }
-          if (config[`comparison_style_${dataPoint.name}`] === "percentage_change") {
-            options[`pos_is_bad_${dataPoint.name}`] = {
-              type: 'boolean',
-              label: `Positive Values are Bad`,
-              section: 'Comparison',
-              default: false,
-              order: 10 * index + 2,
-            }
+        }
+        if (config[`comparison_show_label_${dataPoint.name}`]) {
+          options[`comparison_label_${dataPoint.name}`] = {
+            type: 'string',
+            label: `${dataPoint.label} - Label`,
+            placeholder: dataPoint.label,
+            section: 'Comparison',
+            order: 10 * index + 4,
           }
-          if (config[`comparison_show_label_${dataPoint.name}`]) {
-            options[`comparison_label_${dataPoint.name}`] = {
-              type: 'string',
-              label: `${dataPoint.label} - Label`,
-              placeholder: dataPoint.label,
-              section: 'Comparison',
-              order: 10 * index + 4,
-            }
-            options[`comparison_label_placement_${dataPoint.name}`] = {
-              type: 'string',
-              label: `${dataPoint.label} - Label Placement`,
-              display: 'select',
-              values: [
-                {'Above': 'above'},
-                {'Below': 'below'},
-                {'Left': 'left'},
-                {'Right': 'right'},
-              ],
-              default: 'below',
-              section: 'Comparison',
-              order: 10 * index + 5,
-            }
-            if (config[`comparison_style_${dataPoint.name}`] === "value" ||
-                config[`comparison_style_${dataPoint.name}`] === "calculate_progress_perc") {
-              options[`comp_value_format_${dataPoint.name}`] = {
-                type: 'string',
-                label: `Comparison Value Format`,
-                placeholder: "Spreadsheet-style format code",
-                section: 'Comparison',
-                default: "",
-                order: 10 * index + 6
-              }
-            }
+          options[`comparison_label_placement_${dataPoint.name}`] = {
+            type: 'string',
+            label: `${dataPoint.label} - Label Placement`,
+            display: 'select',
+            values: [
+              {'Above': 'above'},
+              {'Below': 'below'},
+              {'Left': 'left'},
+              {'Right': 'right'},
+            ],
+            default: 'below',
+            section: 'Comparison',
+            order: 10 * index + 5,
           }
+          // if (config[`comparison_style_${dataPoint.name}`] === "value" ||
+          //     config[`comparison_style_${dataPoint.name}`] === "calculate_progress_perc") {
+          //   options[`comp_value_format_${dataPoint.name}`] = {
+          //     type: 'string',
+          //     label: `Comparison Value Format`,
+          //     placeholder: "Spreadsheet-style format code",
+          //     section: 'Comparison',
+          //     default: "",
+          //     order: 10 * index + 6
+          //   }
+          // }
         }
       }
     })
@@ -293,28 +322,29 @@ looker.plugins.visualizations.add({
       currentConfig = Object.assign({}, config)
     }
 
-    let valuesToComparisonsMap = {}
-    let lastDataPointIndex = -1
-    const fullValues = dataPoints.filter((dataPoint, index) => {
-      if (config[`show_comparison_${dataPoint.name}`] !== true) {
-        lastDataPointIndex++
-        return true
-      } else {
-        valuesToComparisonsMap[lastDataPointIndex] = index
-      }
-      return false
-    }).map((fullValue, index) => {
-      const comparisonIndex = valuesToComparisonsMap[index]
-      if (comparisonIndex) {
-        fullValue.comparison = dataPoints[comparisonIndex]
-      }
-      return fullValue;
-    })
+    // let valuesToComparisonsMap = {}
+    // let lastDataPointIndex = -1
+    //INFO: The code here was removing and determining the value to compare, going to change this
+    // const fullValues = dataPoints.filter((dataPoint, index) => {
+    //   if (config[`show_comparison_${dataPoint.name}`] !== true) {
+    //     lastDataPointIndex++
+    //     return true
+    //   } else {
+    //     valuesToComparisonsMap[lastDataPointIndex] = index
+    //   }
+    //   return false
+    // }).map((fullValue, index) => {
+    //   const comparisonIndex = valuesToComparisonsMap[index]
+    //   if (comparisonIndex) {
+    //     fullValue.comparison = dataPoints[comparisonIndex]
+    //   }
+    //   return fullValue;
+    // })
 
     this.chart = ReactDOM.render(
       <MultipleValue
         config={config}
-        data={fullValues}
+        data={dataPoints}
       />,
       element
     );
